908146934b0d4417a8c0bd35ef484c02d8185b0a
Rabbani955 <srabbani955@gmail.com>
2026-04-26 17:05:45 +0530

    fix cors for vercel frontend

--- src/main/java/com/marella/security/SecurityConfig.java
+++ src/main/java/com/marella/security/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
             "http://localhost:5173",
             "http://127.0.0.1:5173",
             "https://hotel-marella-royal-inn.onrender.com",
-            "https://*.onrender.com"   // 🔥 IMPORTANT for flexibility
+            "https://*.onrender.com" ,  // 🔥 IMPORTANT for flexibility
+            "https://*.vercel.app"   // ✅ ADD THIS LINE
         ));
 
         config.setAllowedHeaders(List.of("*"));
